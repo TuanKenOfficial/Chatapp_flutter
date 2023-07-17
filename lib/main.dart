221889d251a594068058eb24981a344ecabd54b4
TuanKenOfficial <nhatyeuthu2013@gmail.com>
2023-07-17 22:04:57 +0700

    Moi code 1 vai thu trong main.dart va home_page.dart va mot vai thu muc

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:chatapp/pages/home_page.dart';
+import 'package:chatapp/shared/constants.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  if (kIsWeb) {
+    //run the web
+    await Firebase.initializeApp(
+        options: FirebaseOptions(
+            apiKey: Constants.apiKey,
+            appId: Constants.appId,
+            messagingSenderId: Constants.messagingSenderId,
+            projectId: Constants.projectId));
+  } 
+  else {
+    //run android and ios
+    await Firebase.initializeApp();
+  }
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
---
+++ lib/shared/constants.dart
@@ -0,0 +1,6 @@
+class Constants {
+  static String appId = "1:159495222381:web:f5d41b71600b3462793b64";
+  static String apiKey = "AIzaSyC86-8VzSz16ZyvUKSfLVPL4j8Z1nAUtvc"; 
+  static String messagingSenderId = "159495222381"; 
+  static String projectId = "chatappflutter-3dae4"; 
+}
